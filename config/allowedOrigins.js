687b8db5a8b25b424861d3957073ce6e974ede81
ahsan <makabir98@yahoo.com>
2024-06-15 11:32:15 +0600

    initial server setup done

---
+++ config/allowedOrigins.js
@@ -0,0 +1,12 @@
+const allowedOrigins = [
+    "https://www.yoursite.com",
+    "http://localhost:5500",
+    "http://127.0.0.1:8000",
+    "http://localhost:3500",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:4173",
+    "http://119.148.49.166:80"
+];
+
+module.exports = allowedOrigins;
--- index.js
+++ index.js
@@ -2,4 +2,29 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const { logger } = require("./middleware/log/logEvents");
+const errorHandler = require("./middleware/errorHandle/errorHandler");
+const credentials = require("./middleware/credential/credentials");
+const corsOptions = require("./config/corsOptions");
+const cmhccRoutes = require("./routes/cmhccRoutes")
+require('dotenv').config();
 const PORT = process.env.PORT;
+
+app.use(logger);
+app.use(credentials);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Hello from root \n");
+});
+
+app.use('/cmhcc-api', cmhccRoutes);
+
+app.all("*", (req, res) => {
+    res.status(404);
+});
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
